af4ab1f41015f57821ce8017193448ccc2d0e8d1
Leehojun12 <dlghwns82@naver.com>
2022-05-28 16:40:45 +0900

    DTO content

--- src/main/java/com/hype/dto/BlacklistDTO.java
+++ src/main/java/com/hype/dto/BlacklistDTO.java
@@ -2,4 +2,40 @@ package com.hype.dto;
 
 public class BlacklistDTO {
 
+	private String reg_date;
+	private String user_id;
+	private String content;
+	
+	public BlacklistDTO() {}
+	public BlacklistDTO(String reg_date, String user_id, String content) {
+		super();
+		this.reg_date = reg_date;
+		this.user_id = user_id;
+		this.content = content;
+	}
+	public String getReg_date() {
+		return reg_date;
+	}
+	public void setReg_date(String reg_date) {
+		this.reg_date = reg_date;
+	}
+	public String getUser_id() {
+		return user_id;
+	}
+	public void setUser_id(String user_id) {
+		this.user_id = user_id;
+	}
+	public String getContent() {
+		return content;
+	}
+	public void setContent(String content) {
+		this.content = content;
+	}
+	@Override
+	public String toString() {
+		return reg_date + " : " + user_id + " : "+ content;
+	}
+	
+	
+	
 }
--- src/main/java/com/hype/dto/ProductDTO.java
+++ src/main/java/com/hype/dto/ProductDTO.java
@@ -1,5 +1,56 @@
 package com.hype.dto;
 
 public class ProductDTO {
-
+	private int product_seq;
+	private String product_name;
+	private int product_price;
+	private String category;
+	private String product_content;
+	
+	public ProductDTO() {}
+	public ProductDTO(int product_seq, String product_name, int product_price, String categoy, String product_content) {
+		super();
+		this.product_seq = product_seq;
+		this.product_name = product_name;
+		this.product_price = product_price;
+		this.category = categoy;
+		this.product_content = product_content;
+	}
+	public int getProduct_seq() {
+		return product_seq;
+	}
+	public void setProduct_seq(int product_seq) {
+		this.product_seq = product_seq;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public int getProduct_price() {
+		return product_price;
+	}
+	public void setProduct_price(int product_price) {
+		this.product_price = product_price;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getProduct_content() {
+		return product_content;
+	}
+	public void setProduct_content(String product_content) {
+		this.product_content = product_content;
+	}
+	@Override
+	public String toString() {
+		return product_seq + " : " + product_name + " : "
+				+ product_price + " : " + category + " : " + product_content;
+	}
+	
+	
 }
