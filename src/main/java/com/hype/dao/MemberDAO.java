81a4de99693a5e15b107dc1b34ac270a7e50c4fb
cococonutnutnut <kgh198621@naver.com>
2022-05-31 04:35:10 +0900

    team A signup create

--- src/main/java/com/hype/dao/MemberDAO.java
+++ src/main/java/com/hype/dao/MemberDAO.java
@@ -2,6 +2,7 @@ package com.hype.dao;
 
 import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 
 import javax.naming.Context;
 import javax.naming.InitialContext;
@@ -24,25 +25,45 @@ private BasicDataSource bds;
 	}
 	
 	public int signup(MemberDTO dto) throws Exception{
-		String sql = "insert into tbl_member values(?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+		String sql = "insert into tbl_member values(?, ?, ?, ?, ?, ?, ?, ?, ?, default)";
 		
 		try(Connection con = bds.getConnection();
 			PreparedStatement pstmt = con.prepareStatement(sql)){
 			
-			pstmt.setString(1, dto.getId());
-			pstmt.setString(2, dto.getPw());
-			pstmt.setString(3, dto.getNickname());
-			pstmt.setString(4, dto.getPhone());
-			pstmt.setString(5, dto.getPostCode());
-			pstmt.setString(6, dto.getRoadAddr());
-			pstmt.setString(7, dto.getDetailAddr());
-			pstmt.setString(8, dto.getExtraAddr());
-			pstmt.setString(8, dto.getExtraAddr());
-			pstmt.setString(8, dto.getExtraAddr());
+			pstmt.setString(1, dto.getUser_id());
+			pstmt.setString(2, dto.getUser_password());
+			pstmt.setString(3, dto.getUser_name());
+			pstmt.setString(4, dto.getUser_date());
+			pstmt.setInt(5, dto.getUser_postCode());
+			pstmt.setString(6, dto.getUser_roadAddr());
+			pstmt.setString(7, dto.getUser_detailAddr());
+			pstmt.setInt(8, dto.getUser_phone());
+			pstmt.setString(9, dto.getUser_email());
 			
 			int rs = pstmt.executeUpdate();
 			return rs;
 		}
 		
+	}
+	
+	public boolean checkId(String id) throws Exception{
+		String sql = "select count(*) from tbl_member where user_id = ?";
+		
+		try(Connection con = bds.getConnection();
+			PreparedStatement pstmt = con.prepareStatement(sql)){
+			
+			pstmt.setString(1, id);
+			ResultSet rs = pstmt.executeQuery();
+			
+			int result = 0;
+			if(rs.next()) {
+				result = rs.getInt(1);
+			}
+			if(result == 0) {
+				return true;
+			}else {
+				return false;
+			}
+		}
 	}
 }
